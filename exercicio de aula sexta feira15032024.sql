a61bf442f9f54590edf46ddecc7c960aade7952e
Gelson Lima de Souza Junior <153616550+gelsonli@users.noreply.github.com>
2024-03-15 11:33:04 -0300

    Add files via upload

---
+++ exercicio de aula sexta feira15032024.sql
@@ -0,0 +1,23 @@
+-- Gelson Lima de Souza Junior
+--Questão 1
+use sistema_biblioteca
+select * from LIVRO_BIBLIOTECA where cnpj = 1234567800123;
+
+--Questao 2
+select f.nome
+from BIBLIOTECARIO f;
+
+--Questao3
+select * from LIVRO_BIBLIOTECA where numero_registro = 10;
+
+--Questão 4
+select * from EVENTO where tipo = 'workshop'and custo > 150.00
+
+--Questão 5
+select LIVRO.titulo
+from LIVRO
+join tecnologia on tecnologia.numero_registro = LIVRO.numero_registro;
+
+--Questão 6
+
+
